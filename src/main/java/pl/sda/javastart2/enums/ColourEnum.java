3420451981055176d1328345a482fe54a06bfc74
sdakf <sdakftraining@gmail.com>
2018-04-14 18:29:43 +0200

    init

---
+++ src/main/java/pl/sda/javastart2/enums/ColourEnum.java
@@ -0,0 +1,21 @@
+package pl.sda.javastart2.enums;
+
+public enum ColourEnum {
+    WHITE(0, "biały"), BLACK(1, "czarny");
+
+    private int numberValue;
+    private String plName;
+
+    ColourEnum(int numberValue, String plName) {
+        this.numberValue = numberValue;
+        this.plName = plName;
+    }
+
+    public int getNumberValue() {
+        return numberValue;
+    }
+
+    public String getPlName() {
+        return plName;
+    }
+}
