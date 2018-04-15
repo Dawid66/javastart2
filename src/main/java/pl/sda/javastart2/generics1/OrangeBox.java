4c0b5fb082d408dd0b1fbf6737af1e903abe3907
sdakf <sdakftraining@gmail.com>
2018-04-15 14:45:14 +0200

    generics

---
+++ src/main/java/pl/sda/javastart2/generics1/OrangeBox.java
@@ -0,0 +1,14 @@
+package pl.sda.javastart2.generics1;
+
+public class OrangeBox {
+
+    private Orange orange;
+
+    public OrangeBox(Orange orange) {
+        this.orange = orange;
+    }
+
+    public Orange getOrange() {
+        return orange;
+    }
+}
